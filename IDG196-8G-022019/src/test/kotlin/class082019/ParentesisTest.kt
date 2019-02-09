ef5f048c2363181f29c30c017f0716951bad8bc1
Luis <bookmagician>
2019-02-08 19:17:24 -0800

    Ejercicio Parentesis

--- IDG196-8G-022019/src/test/kotlin/class082019/ParentesisTest.kt
+++ IDG196-8G-022019/src/test/kotlin/class082019/ParentesisTest.kt
@@ -1,10 +1,20 @@
 package class082019
 
+import clase082019.Parentesis
+import org.junit.Test
+
 class ParentesisTest {
-    private val parentesis = Parentesis{
-        @Test
-        fun `returns false when case is wrong`(){
-            val test1 = 
-        }
+    private val parentesis = Parentesis()
+
+    @Test
+
+    fun `returns false if starts with )`(){
+
     }
+
+    fun `returns false if starts with }`(){
+
+    }
+
+
 }
